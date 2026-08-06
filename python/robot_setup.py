44c41b17932dde428cc2406b5ece5fa9b6643d05
Jean Biboum <bibsjp@gmail.com>
2026-08-06 23:33:33 +0200

    feat: add robot_setup

---
+++ python/robot_setup.py
@@ -0,0 +1,14 @@
+#Configuration du robot
+nom = input("Quel est le nom du robot ?")
+vitesse = input("Quelle est sa vitesse maximale ?")
+poids = input("Quel est son poids ?")
+autonomie = input("Quelle est son autonomie ?")
+
+print(f""" ======Configuration du robot======
+Nom : {nom}
+Vitesse maximale : {vitesse}
+Poids : {poids}
+Autonomie : {autonomie}
+===================================
+""")
+
